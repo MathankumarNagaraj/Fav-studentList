8b62a7030087708b8259e04e6dc6f72e089cea16
Mathankumar N <mathankumarn1104@gmail.com>
2024-11-22 23:18:06 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useState } from "react";
+import List from "./List";
+import FavouriteStudents from "./FavouriteStudents";
+
+function App() {
+   const [favoriteList, setFavoriteList] = useState([]);
+
+   // Add a student to the favorite list
+   const addToFavorites = (student) => {
+      setFavoriteList((prev) => [...prev, student]);
+   };
+
+   // Remove a student from the favorite list
+   const removeFromFavorites = (student) => {
+      setFavoriteList((prev) => prev.filter((fav) => fav !== student));
+   };
+
+   return (
+      <Router>
+         <Routes>
+            <Route
+               path="/"
+               element={
+                  <List
+                     favoriteList={favoriteList}
+                     addToFavorites={addToFavorites}
+                  />
+               }
+            />
+            <Route
+               path="/favourite-students"
+               element={
+                  <FavouriteStudents
+                     favoriteList={favoriteList}
+                     removeFromFavorites={removeFromFavorites}
+                  />
+               }
+            />
+         </Routes>
+      </Router>
+   );
+}
+
+export default App;
